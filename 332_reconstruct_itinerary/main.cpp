f9623bc4bc6e2f6dc8c8d4928996c8e4796dfb5f
dbwolfe <dbwolfe@allegromicro.com>
2024-02-16 15:13:38 -0800

    Added recursive solution to find itinerary

--- 332_reconstruct_itinerary/main.cpp
+++ 332_reconstruct_itinerary/main.cpp
@@ -3,6 +3,8 @@
 #include <string>
 #include <exception>
 #include <list>
+#include <unordered_map>
+#include <algorithm>
 
 class Itinerary
 {
@@ -155,6 +157,62 @@ std::vector<std::string> findItinerary(std::vector<std::vector<std::string>>& ti
     return ChooseItinerary(itineraryList, tickets.size());
 }
 
+int GetNextTicketRecursive(
+    std::string &from, 
+    std::unordered_map<std::string, std::vector<std::string>> &ticketMap, 
+    std::vector<std::string> &answer, 
+    int numTickets)
+{
+    // If the answer already contains the maximum number of stops, return 1.
+    if(answer.size() == (numTickets + 1)) {
+        return 1;
+    }
+
+    // Given the current "from" location, investigate each possible "to" destination.
+    for(int i = 0; i < ticketMap[from].size(); i++){
+        std::string to = ticketMap[from][i];
+
+        // If this destination was already used, try the next.
+        if(to == "$$$") {
+            continue;
+        }
+
+        // Otherwise, mark this destination as visited.
+        ticketMap[from][i] = "$$$";
+        answer.push_back(to);
+
+        // Check whether by using this ticket next we are ultimately able to use every ticket.
+        if( GetNextTicketRecursive(to, ticketMap, answer, numTickets) ) return 1;
+        else{
+            ticketMap[from][i] = to; // backtrack
+            answer.pop_back();
+        }
+    }
+
+    return 0;
+}
+
+std::vector<std::string> findItinerary1(std::vector<std::vector<std::string>>& tickets) 
+{
+    std::unordered_map<std::string, std::vector<std::string>> ticketMap;
+    std::vector<std::string> answer;
+    std::string start = "JFK";
+
+    // Add all tickets to the map.
+    for(auto ticket : tickets ) {
+        ticketMap[ticket[0]].push_back(ticket[1]);
+    }
+
+    // Sort tickets by "to" in lexicographical order.
+    for(auto &x : ticketMap){
+        std::sort(x.second.begin(), x.second.end());
+    }
+
+    answer.push_back(start);
+    GetNextTicketRecursive(start, ticketMap, answer, tickets.size());
+    return answer;
+}
+
 int main()
 {
     std::vector<std::vector<std::string>> tickets0 = { {"MUC", "LHR"}, {"JFK", "MUC"}, {"SFO", "SJC"}, {"LHR", "SFO"} };
@@ -162,7 +220,7 @@ int main()
     std::vector<std::vector<std::string>> tickets2 = { {"JFK","KUL"}, {"JFK","NRT"}, {"NRT","JFK"} };
     std::vector<std::vector<std::string>> tickets3 = { {"EZE","TIA"},{"EZE","HBA"},{"AXA","TIA"},{"JFK","AXA"},{"ANU","JFK"},{"ADL","ANU"},{"TIA","AUA"},{"ANU","AUA"},{"ADL","EZE"},{"ADL","EZE"},{"EZE","ADL"},{"AXA","EZE"},{"AUA","AXA"},{"JFK","AXA"},{"AXA","AUA"},{"AUA","ADL"},{"ANU","EZE"},{"TIA","ADL"},{"EZE","ANU"},{"AUA","ANU"} };
     std::vector<std::vector<std::string>> tickets4 = { {"AXA","EZE"},{"EZE","AUA"},{"ADL","JFK"},{"ADL","TIA"},{"AUA","AXA"},{"EZE","TIA"},{"EZE","TIA"},{"AXA","EZE"},{"EZE","ADL"},{"ANU","EZE"},{"TIA","EZE"},{"JFK","ADL"},{"AUA","JFK"},{"JFK","EZE"},{"EZE","ANU"},{"ADL","AUA"},{"ANU","AXA"},{"AXA","ADL"},{"AUA","JFK"},{"EZE","ADL"},{"ANU","TIA"},{"AUA","JFK"},{"TIA","JFK"},{"EZE","AUA"},{"AXA","EZE"},{"AUA","ANU"},{"ADL","AXA"},{"EZE","ADL"},{"AUA","ANU"},{"AXA","EZE"},{"TIA","AUA"},{"AXA","EZE"},{"AUA","SYD"},{"ADL","JFK"},{"EZE","AUA"},{"ADL","ANU"},{"AUA","TIA"},{"ADL","EZE"},{"TIA","JFK"},{"AXA","ANU"},{"JFK","AXA"},{"JFK","ADL"},{"ADL","EZE"},{"AXA","TIA"},{"JFK","AUA"},{"ADL","EZE"},{"JFK","ADL"},{"ADL","AXA"},{"TIA","AUA"},{"AXA","JFK"},{"ADL","AUA"},{"TIA","JFK"},{"JFK","ADL"},{"JFK","ADL"},{"ANU","AXA"},{"TIA","AXA"},{"EZE","JFK"},{"EZE","AXA"},{"ADL","TIA"},{"JFK","AUA"},{"TIA","EZE"},{"EZE","ADL"},{"JFK","ANU"},{"TIA","AUA"},{"EZE","ADL"},{"ADL","JFK"},{"ANU","AXA"},{"AUA","AXA"},{"ANU","EZE"},{"ADL","AXA"},{"ANU","AXA"},{"TIA","ADL"},{"JFK","ADL"},{"JFK","TIA"},{"AUA","ADL"},{"AUA","TIA"},{"TIA","JFK"},{"EZE","JFK"},{"AUA","ADL"},{"ADL","AUA"},{"EZE","ANU"},{"ADL","ANU"},{"AUA","AXA"},{"AXA","TIA"},{"AXA","TIA"},{"ADL","AXA"},{"EZE","AXA"},{"AXA","JFK"},{"JFK","AUA"},{"ANU","ADL"},{"AXA","TIA"},{"ANU","AUA"},{"JFK","EZE"},{"AXA","ADL"},{"TIA","EZE"},{"JFK","AXA"},{"AXA","ADL"},{"EZE","AUA"},{"AXA","ANU"},{"ADL","EZE"},{"AUA","EZE"}};
-    std::vector<std::string>& itinerary = findItinerary(tickets4);
+    std::vector<std::string>& itinerary = findItinerary1(tickets1);
 
     std::cout << "itinerary: ";
     for (auto stop : itinerary) {
