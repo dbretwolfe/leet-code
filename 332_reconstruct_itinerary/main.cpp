aac61ffcf82535ca1e76528b3db77c17ef0ecb39
dbwolfe <dbwolfe@allegromicro.com>
2024-02-15 17:24:14 -0800

    First pass at std::list based itinerary collection with removal

--- 332_reconstruct_itinerary/main.cpp
+++ 332_reconstruct_itinerary/main.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <exception>
+#include <list>
 
 class Itinerary
 {
 public:
     Itinerary(size_t numTickets) { used.resize(numTickets); }
-
-    
     std::vector<std::string> nodes;
     std::vector<bool> used;
     int usedCount = 0;
@@ -30,21 +30,6 @@ void AddNextDestination(
 
     itinerary.used[nextTicket] = true;
     itinerary.usedCount++;
-}
-
-// Find the minimum lexical value of the different ticket destinations.
-int ChooseTicket(std::vector<std::vector<std::string>>& tickets, std::vector<int> ticketNums)
-{
-    int ticket = (ticketNums.empty() == true) ? -1 : ticketNums[0];
-
-    for (int i : ticketNums) {
-        int compVal = tickets[i][1].compare(tickets[ticket][1]);
-        if (compVal < 0) {
-            ticket = i;
-        }
-    }
-
-    return ticket;
 }
 
 // Find any tickets that will continue the itenerary.
@@ -72,19 +57,74 @@ std::vector<int> GetNextTickets(
     return nextTickets;
 }
 
+// Find the minimum lexical value of the different ticket destinations.
+int LexicalCompareItinerary(std::vector<std::string>& a, std::vector<std::string>& b)
+{
+    if ((a.empty() == true) || (a.size() != b.size())) {
+        throw std::invalid_argument("Invalid inputs for lexical comparison!");
+    }
+
+    int compVal = 0;
+
+    for (int i = 0; i < a.size(); i++) {
+        compVal = a[i].compare(b[i]);
+        if (compVal != 0) {
+            break;
+        }
+    }
+
+    return compVal;
+}
+
+std::vector<std::string>& ChooseItinerary(std::list<Itinerary>& itineraryList, int numTickets)
+{
+    if (itineraryList.empty() == true) {
+        throw std::invalid_argument("Itinerary list is empty!");
+    }
+    else if (itineraryList.size() == 1) {
+        return itineraryList.begin()->nodes;
+    }
+
+    auto lowestLexical = itineraryList.begin();
+    auto it = itineraryList.begin();
+
+    for (; it != itineraryList.end(); it++) {
+        if (it->usedCount != numTickets) {
+            continue;
+        }
+
+        if (lowestLexical->usedCount != numTickets) {
+            lowestLexical = it;
+        }
+        else {
+            int compVal = LexicalCompareItinerary(lowestLexical->nodes, it->nodes);
+            lowestLexical = (compVal > 0) ? it : lowestLexical;
+        }
+    }
+
+    return lowestLexical->nodes;
+}
+
 std::vector<std::string> findItinerary(std::vector<std::vector<std::string>>& tickets) 
 {
-    std::vector<Itinerary> itineraryList = { Itinerary(tickets.size()) };
+    std::list<Itinerary> itineraryList = { Itinerary(tickets.size()) };
 
     while (1) {
         int passCount = 0;
-        for (int i = 0; i < itineraryList.size(); i++) {
-            std::vector<int> nextTickets = GetNextTickets(itineraryList[i], tickets);
+        auto it = itineraryList.begin();
+
+        for(;it != itineraryList.end(); it++) {
+            std::vector<int> nextTickets = GetNextTickets(*it, tickets);
 
             // If there are no ticket matches, increment the pass count and continue.
+            // If this itinerary runs out of ticket combinations before using all tickets, remove it from the list.
             if (nextTickets.empty() == true) {
-                passCount++;
-                continue;
+                if (it->usedCount < tickets.size()) {
+                    itineraryList.erase(it);
+                }
+                else {
+                    passCount++;
+                }
             }
             // If there is one ticket match, add that ticket to the current itinerary.
             // If there are more than one matches, copy the current itinerary for each match and
@@ -92,13 +132,13 @@ std::vector<std::string> findItinerary(std::vector<std::vector<std::string>>& ti
             else {
                 // Create any copies before modifying the current itinerary.
                 for (int j = 1 ; j < nextTickets.size(); j ++) {
-                    Itinerary newItinerary(itineraryList[i]);
+                    Itinerary newItinerary(*it);
                     itineraryList.push_back(newItinerary);
                     AddNextDestination(itineraryList.back(), tickets, nextTickets[j]);
                 }
 
                 // Add the first match to the current itenerary. 
-                AddNextDestination(itineraryList[i], tickets, nextTickets[0]);
+                AddNextDestination(*it, tickets, nextTickets[0]);
             }
         }
 
@@ -107,9 +147,9 @@ std::vector<std::string> findItinerary(std::vector<std::vector<std::string>>& ti
         {
             break;
         }
-    }   
+    } 
 
-    return std::vector<std::string>();
+    return ChooseItinerary(itineraryList, tickets.size());
 }
 
 int main()
@@ -117,7 +157,9 @@ int main()
     std::vector<std::vector<std::string>> tickets0 = { {"MUC", "LHR"}, {"JFK", "MUC"}, {"SFO", "SJC"}, {"LHR", "SFO"} };
     std::vector<std::vector<std::string>> tickets1 = { {"JFK","SFO"}, {"JFK","ATL"}, {"SFO","ATL"}, {"ATL","JFK"}, {"ATL","SFO"} };
     std::vector<std::vector<std::string>> tickets2 = { {"JFK","KUL"}, {"JFK","NRT"}, {"NRT","JFK"} };
-    std::vector<std::string>& itinerary = findItinerary(tickets2);
+    std::vector<std::vector<std::string>> tickets3 = { {"EZE","TIA"},{"EZE","HBA"},{"AXA","TIA"},{"JFK","AXA"},{"ANU","JFK"},{"ADL","ANU"},{"TIA","AUA"},{"ANU","AUA"},{"ADL","EZE"},{"ADL","EZE"},{"EZE","ADL"},{"AXA","EZE"},{"AUA","AXA"},{"JFK","AXA"},{"AXA","AUA"},{"AUA","ADL"},{"ANU","EZE"},{"TIA","ADL"},{"EZE","ANU"},{"AUA","ANU"} };
+    std::vector<std::vector<std::string>> tickets4 = { {"AXA","EZE"},{"EZE","AUA"},{"ADL","JFK"},{"ADL","TIA"},{"AUA","AXA"},{"EZE","TIA"},{"EZE","TIA"},{"AXA","EZE"},{"EZE","ADL"},{"ANU","EZE"},{"TIA","EZE"},{"JFK","ADL"},{"AUA","JFK"},{"JFK","EZE"},{"EZE","ANU"},{"ADL","AUA"},{"ANU","AXA"},{"AXA","ADL"},{"AUA","JFK"},{"EZE","ADL"},{"ANU","TIA"},{"AUA","JFK"},{"TIA","JFK"},{"EZE","AUA"},{"AXA","EZE"},{"AUA","ANU"},{"ADL","AXA"},{"EZE","ADL"},{"AUA","ANU"},{"AXA","EZE"},{"TIA","AUA"},{"AXA","EZE"},{"AUA","SYD"},{"ADL","JFK"},{"EZE","AUA"},{"ADL","ANU"},{"AUA","TIA"},{"ADL","EZE"},{"TIA","JFK"},{"AXA","ANU"},{"JFK","AXA"},{"JFK","ADL"},{"ADL","EZE"},{"AXA","TIA"},{"JFK","AUA"},{"ADL","EZE"},{"JFK","ADL"},{"ADL","AXA"},{"TIA","AUA"},{"AXA","JFK"},{"ADL","AUA"},{"TIA","JFK"},{"JFK","ADL"},{"JFK","ADL"},{"ANU","AXA"},{"TIA","AXA"},{"EZE","JFK"},{"EZE","AXA"},{"ADL","TIA"},{"JFK","AUA"},{"TIA","EZE"},{"EZE","ADL"},{"JFK","ANU"},{"TIA","AUA"},{"EZE","ADL"},{"ADL","JFK"},{"ANU","AXA"},{"AUA","AXA"},{"ANU","EZE"},{"ADL","AXA"},{"ANU","AXA"},{"TIA","ADL"},{"JFK","ADL"},{"JFK","TIA"},{"AUA","ADL"},{"AUA","TIA"},{"TIA","JFK"},{"EZE","JFK"},{"AUA","ADL"},{"ADL","AUA"},{"EZE","ANU"},{"ADL","ANU"},{"AUA","AXA"},{"AXA","TIA"},{"AXA","TIA"},{"ADL","AXA"},{"EZE","AXA"},{"AXA","JFK"},{"JFK","AUA"},{"ANU","ADL"},{"AXA","TIA"},{"ANU","AUA"},{"JFK","EZE"},{"AXA","ADL"},{"TIA","EZE"},{"JFK","AXA"},{"AXA","ADL"},{"EZE","AUA"},{"AXA","ANU"},{"ADL","EZE"},{"AUA","EZE"}};
+    std::vector<std::string>& itinerary = findItinerary(tickets4);
 
     std::cout << "itinerary: ";
     for (auto stop : itinerary) {
